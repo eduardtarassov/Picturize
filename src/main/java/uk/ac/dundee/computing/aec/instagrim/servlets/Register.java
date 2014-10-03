7b5f3a74133bf2df38092910eb78e3d66d377c19
eduardtarassov <eduardtarassov@gmail.com>
2014-10-03 23:07:22 +0100

    So far the connection with MySQL works perfectly. Login and Registration are repaired. Next step is to set up a proper redirection to a page with the information that user has already been logon.

--- src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/Register.java
+++ src/main/java/uk/ac/dundee/computing/aec/instagrim/servlets/Register.java
@@ -6,9 +6,9 @@
 
 package uk.ac.dundee.computing.aec.instagrim.servlets;
 
-import com.datastax.driver.core.Cluster;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.Enumeration;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -16,8 +16,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import uk.ac.dundee.computing.aec.instagrim.lib.CassandraHosts;
-import uk.ac.dundee.computing.aec.instagrim.models.User;
+import javax.servlet.http.HttpSession;
+
+
+import uk.ac.dundee.computing.aec.instagrim.db.utils.ConnectionUtil;
 
 /**
  *
@@ -25,45 +27,133 @@ import uk.ac.dundee.computing.aec.instagrim.models.User;
  */
 @WebServlet(name = "Register", urlPatterns = {"/Register"})
 public class Register extends HttpServlet {
-    Cluster cluster=null;
+
+    private static final String REGISTRATION_PAGE = "/register.jsp";
+    private static final String HOME_PAGE = "/index.jsp";
+    private static final String INSERT_QUERY_START = "insert into users values ('";
+
     public void init(ServletConfig config) throws ServletException {
         // TODO Auto-generated method stub
-        cluster = CassandraHosts.getCluster();
+    }
+
+    /**
+     * Internally called to validate the request data. This method does a simple
+     * check for userName and password to be entered. However
+     * it can be extended to add more complex check at the server side like email address validity etc.
+     * @param request - HttpServletRequest object
+     * @return boolean indicating success / failure of the validation
+     */
+    private boolean validateData(HttpServletRequest request) {
+        boolean isValid = false;
+        String strUserName = request.getParameter("username");
+        String strPassword = request.getParameter("password");
+
+        if(strUserName!=null && !strUserName.equals("") && strPassword!=null && !strPassword.equals("")) {
+            isValid = true;
+        }
+        return isValid;
+    }
+
+    /**
+     * Fancy way of looking through all the request parameters and setting in request Attributes.
+     * @param request
+     */
+    private void setRequestAttributes(HttpServletRequest request) {
+        Enumeration<String>  enumKeys =  request.getParameterNames();
+        while(enumKeys.hasMoreElements()) {
+            String key  = enumKeys.nextElement();
+            request.setAttribute(key, request.getParameter(key))  ;
+        }
     }
 
 
 
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+    /*
+    * fetches the request parameters and generates the insert query
+    * which will be passed on to the executeQuery method of the ConnectionUtil.
+    * Below is the code for the method
      */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String username=request.getParameter("username");
-        String password=request.getParameter("password");
-        
-        User us=new User();
-        us.setCluster(cluster);
-        us.RegisterUser(username, password);
-        
-	response.sendRedirect("/Instagrim");
-        
+    private String generateInsertQuery(HttpServletRequest request) {
+        String strUserName = request.getParameter("username");
+        String strPassword = request.getParameter("password");
+        String strFirstName = request.getParameter("first_name");
+        String strLastName = request.getParameter("last_name");
+        String strEmail = request.getParameter("email_address");
+        String strSecretQuestion = request.getParameter("secret_question");
+        String strSecretAnswer = request.getParameter("secret_answer");
+
+        StringBuffer strQuery = new StringBuffer(INSERT_QUERY_START);
+        strQuery.append(strUserName);
+        strQuery.append("', '");
+        strQuery.append(strPassword);
+        strQuery.append("', '");
+        strQuery.append(strFirstName);
+        strQuery.append("', '");
+        strQuery.append(strLastName);
+        strQuery.append("', '");
+        strQuery.append(strEmail);
+        strQuery.append("', '");
+        strQuery.append(strSecretQuestion);
+        strQuery.append("', '");
+        strQuery.append(strSecretAnswer);
+        strQuery.append("')");
+
+        System.out.println("Insert query : " + strQuery.toString());
+
+        return strQuery.toString();
+
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+    protected void doPost(HttpServletRequest request, HttpServletResponse
+            response) throws ServletException, IOException {
 
+        String strUserMsg = null;
+        HttpSession session = request.getSession();
+        RequestDispatcher reqDisp =  request.getRequestDispatcher(REGISTRATION_PAGE);
+
+        try {
+            //Check if data is valid
+            if(validateData(request)) {
+                ConnectionUtil.executeQuery(generateInsertQuery(request));
+                System.out.println("Insert into database successful");
+                session.setAttribute("username", request.getParameter("username"));
+                response.sendRedirect(HOME_PAGE);
+            } else {//If data is invalid
+                strUserMsg = "Username and Password cannot be empty";
+
+                setRequestAttributes(request);
+                request.setAttribute("userMsg", strUserMsg);
+                reqDisp.forward(request, response);
+            }
+
+        } catch(SQLException sqle ) {
+            System.out.println("Unable to register user: "+sqle.getMessage());
+            sqle.printStackTrace();
+            //Check if we are getting duplicate key exception on userName
+            if(sqle.getMessage().indexOf("Duplicate entry")!=-1) {
+                System.out.println("User already exists");
+                strUserMsg = "User name "+request.getParameter("username")+" already " +
+                        "exists. Please try another user name.";
+            } else { //If other SQLException than dup key exception
+                strUserMsg = "Unable to register user "+request.getParameter("userName")+
+                        ". Please try again later.";
+            }
+            setRequestAttributes(request);
+            request.setAttribute("userMsg", strUserMsg);
+            reqDisp.forward(request, response);
+
+        } catch(Exception e) {//If it goes into Exception other than SQLException
+            System.out.println("Unable to register user: "+e.getMessage());
+            strUserMsg = "Unable to register user "+request.getParameter("userName")
+                    +". Please try again later.";
+            setRequestAttributes(request);
+            request.setAttribute("userMsg", strUserMsg);
+            reqDisp.forward(request, response);
+
+        }
+
+
+
+    }
 }
